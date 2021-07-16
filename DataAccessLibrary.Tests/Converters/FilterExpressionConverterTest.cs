780e0133ca420b0d36554d879a7198392e9770df
Roman Sinclair <romanio412@gmail.com>
2021-07-16 04:00:52 +0300

    fully working FilterExpressionConverter Test

--- DataAccessLibrary.Tests/Converters/FilterExpressionConverterTest.cs
+++ DataAccessLibrary.Tests/Converters/FilterExpressionConverterTest.cs
@@ -20,35 +20,35 @@ namespace DataAccessLibrary.Tests
         public void ConvertToSQLEmptyFilterExpressionTest()
         {
             FilterExpression fe = new FilterExpression();
-            Assert.Equal("", FilterExpressionConverter.ConvertToSQL(fe));
-            Assert.Equal("", FilterExpressionConverter.ConvertToSQL(fe, 10));
+            Assert.Equal("", FilterExpressionConverter.ConvertToSQL(fe, ""));
+            Assert.Equal("", FilterExpressionConverter.ConvertToSQL(fe, "this-text-doesn't-matter", 10));
         }
 
         [Fact]
         public void ConvertToSQLSingleQueryFilterExpressionTest()
         {
             FilterExpression fe = new FilterExpression();
-            fe.AddQuery("contact", "name", ConditionOperator.Equal, "John Gray");
+            fe.AddQuery("Contact", "FullName", ConditionOperator.Equal, "John Gray");
 
-            Assert.Equal("WHERE ( name = 'John Gray' ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  ( FullName = 'John Gray' ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
         }
 
         [Fact]
         public void ConvertToSQLMultipleQueriesFilterExpressionTest()
         {
             FilterExpression fe = new FilterExpression();
-            fe.AddQuery("contact", "name", ConditionOperator.Equal, "John Gray");
-            fe.AddQuery("name", ConditionOperator.Like, true, "David Black");
+            fe.AddQuery("Contact", "FullName", ConditionOperator.Equal, "John Gray");
+            fe.AddQuery("FullName", ConditionOperator.Like, true, "David Black");
 
-            Assert.Equal("WHERE ( name = 'John Gray' AND name LIKE '%David Black%' ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  ( FullName = 'John Gray' AND FullName LIKE '%David Black%' ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
 
             fe.QueryOperator = LogicalOperator.Or;
 
-            Assert.Equal("WHERE ( name = 'John Gray' OR name LIKE '%David Black%' ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  ( FullName = 'John Gray' OR FullName LIKE '%David Black%' ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
 
-            fe.AddQuery("age", ConditionOperator.GreaterThan, 6);
+            fe.AddQuery("Age", ConditionOperator.GreaterThan, 6);
 
-            Assert.Equal("WHERE ( name = 'John Gray' OR name LIKE '%David Black%' OR age > 6 ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  ( FullName = 'John Gray' OR FullName LIKE '%David Black%' OR Age > '6' ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
         }
 
         [Fact]
@@ -57,11 +57,11 @@ namespace DataAccessLibrary.Tests
             FilterExpression fe = new FilterExpression(LogicalOperator.Or);
 
             FilterExpression subQuery = new FilterExpression(LogicalOperator.And);
-            subQuery.AddQuery("name", ConditionOperator.Like, "David Black");
-            subQuery.AddQuery("gender", ConditionOperator.Equal, "male");
+            subQuery.AddQuery("FirstName", ConditionOperator.Like, "David");
+            subQuery.AddQuery("GenderId", ConditionOperator.Equal, new EntityReference("Gender", Guid.Parse("824b213f-8ec2-4157-b5f1-96b1ebe89d6c"))); // 824b213f-8ec2-4157-b5f1-96b1ebe89d6c - GUID for Male
             fe.AddSubQuery(subQuery);
 
-            Assert.Equal("WHERE ( ( name LIKE '%David Black%' AND gender = 'male' ) ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  (  ( FirstName LIKE '%David%' AND GenderId = '824b213f-8ec2-4157-b5f1-96b1ebe89d6c' )  ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
         }
 
         [Fact]
@@ -70,47 +70,47 @@ namespace DataAccessLibrary.Tests
             FilterExpression fe = new FilterExpression(LogicalOperator.Or);
 
             FilterExpression subQuery1 = new FilterExpression(LogicalOperator.And);
-            subQuery1.AddQuery("name", ConditionOperator.Like, "David Black");
-            subQuery1.AddQuery("gender", ConditionOperator.Equal, "male");
+            subQuery1.AddQuery("Contact", "FullName", ConditionOperator.Like, "David Black");
+            subQuery1.AddQuery("Contact", "GenderId", ConditionOperator.Equal, new EntityReference("Gender", Guid.Parse("824b213f-8ec2-4157-b5f1-96b1ebe89d6c"))); // 824b213f-8ec2-4157-b5f1-96b1ebe89d6c - GUID for Male
             fe.AddSubQuery(subQuery1);
 
             FilterExpression subQuery2 = new FilterExpression(LogicalOperator.And);
-            subQuery2.AddQuery("name", ConditionOperator.Like, "Sarah");
-            subQuery2.AddQuery("gender", ConditionOperator.Equal, "female");
-            fe.AddSubQuery(subQuery1);
+            subQuery2.AddQuery("Contact", "FirstName", ConditionOperator.Like, "Sarah");
+            subQuery2.AddQuery("Contact", "GenderId", ConditionOperator.Equal, new EntityReference("Gender", Guid.Parse("912b9357-c9c8-40d8-8a81-646057a33053"))); // 912b9357-c9c8-40d8-8a81-646057a33053 - GUID for Female
+            fe.AddSubQuery(subQuery2);
 
-            Assert.Equal("WHERE ( ( name LIKE '%David Black%' AND gender = 'male' ) OR ( name LIKE '%Sarah%' AND gender = 'female' ) ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  (  ( FullName LIKE '%David Black%' AND GenderId = '824b213f-8ec2-4157-b5f1-96b1ebe89d6c' )  OR  ( FirstName LIKE '%Sarah%' AND GenderId = '912b9357-c9c8-40d8-8a81-646057a33053' )  ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact")); // 824B213F-8EC2-4157-B5F1-96B1EBE89D6C - GUID for Male, 912B9357-C9C8-40D8-8A81-646057A33053 - GUID for female
         }
 
         [Fact]
         public void ConvertToSQLComplexFilterExpressionTest()
         {
             FilterExpression fe = new FilterExpression(LogicalOperator.And);
-            fe.AddQuery("city", ConditionOperator.Equal, "New-York");
-            fe.AddQuery("country", ConditionOperator.Equal, "USA");
-            fe.AddQuery("postalcode", ConditionOperator.Like, 12209);
+            fe.AddQuery("CityOfOriginId", ConditionOperator.Equal, new EntityReference("City", Guid.Parse("afbdd272-028b-e811-95b7-08002791e63c"))); // afbdd272-028b-e811-95b7-08002791e63c - Denver
+            fe.AddQuery("CountryOfOriginId", ConditionOperator.Equal, new EntityReference("Country", Guid.Parse("c6257bef-848a-e811-8f78-08002791e63c"))); // c6257bef-848a-e811-8f78-08002791e63c - USA
+            fe.AddQuery("Age", ConditionOperator.Like, 2);
 
             FilterExpression subQuery1 = new FilterExpression(LogicalOperator.Or);
-            subQuery1.AddQuery("name", ConditionOperator.Like, "Joe");
-            subQuery1.AddQuery("name", ConditionOperator.Like, "Mark");
-            subQuery1.AddQuery("name", ConditionOperator.Like, "Charley");
+            subQuery1.AddQuery("FirstName", ConditionOperator.Like, "Joe");
+            subQuery1.AddQuery("FirstName", ConditionOperator.Like, "Mark");
+            subQuery1.AddQuery("FirstName", ConditionOperator.Like, "Charley");
 
             FilterExpression subQuery2 = new FilterExpression(LogicalOperator.And);
-            subQuery2.AddQuery("address", ConditionOperator.Like, "Hanover Sq.");
+            subQuery2.AddQuery("Address", ConditionOperator.Like, "Hanover Sq.");
 
             FilterExpression subQuery3 = new FilterExpression(LogicalOperator.Or);
-            subQuery3.AddQuery("companyname", ConditionOperator.Like, "The Biggest Company");
+            subQuery3.AddQuery("Description", ConditionOperator.Like, "The Biggest Company");
 
             FilterExpression subSubQuery1 = new FilterExpression(LogicalOperator.Or);
-            subSubQuery1.AddQuery("contactname", ConditionOperator.Like, "Paul");
-            subSubQuery1.AddQuery("contactname", ConditionOperator.Like, "Peter");
+            subSubQuery1.AddQuery("ContactRepresentative", ConditionOperator.Like, "Paul");
+            subSubQuery1.AddQuery("ContactRepresentative", ConditionOperator.Like, "Peter");
             subQuery3.AddSubQuery(subSubQuery1);
 
             fe.AddSubQuery(subQuery1);
             fe.AddSubQuery(subQuery2);
             fe.AddSubQuery(subQuery3);
 
-            Assert.Equal("WHERE ( city = 'New-York' AND country = 'USA' AND postalcode LIKE '%12209%' AND ( name LIKE '%Joe%' OR name LIKE '%Mark%' OR name LIKE '%Charley%' ) AND ( address LIKE '%Hanover Sq.%' ) AND ( companyname LIKE 'The Biggest Company' OR ( contactname LIKE '%Paul%' OR contactname LIKE '%Peter%' ) ) ) ", FilterExpressionConverter.ConvertToSQL(fe));
+            Assert.Equal(" WHERE  ( CityOfOriginId = 'afbdd272-028b-e811-95b7-08002791e63c' AND CountryOfOriginId = 'c6257bef-848a-e811-8f78-08002791e63c' AND Age LIKE '%2%' AND  ( FirstName LIKE '%Joe%' OR FirstName LIKE '%Mark%' OR FirstName LIKE '%Charley%' )  AND  ( Address LIKE '%Hanover Sq.%' )  AND  ( Description LIKE '%The Biggest Company%' OR  ( ContactRepresentative LIKE '%Paul%' OR ContactRepresentative LIKE '%Peter%' )  )  ) ", FilterExpressionConverter.ConvertToSQL(fe, "Contact"));
         }
     }
 }
