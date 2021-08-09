35990fb383c523ebf5c626a05a8d94835a5dcaaa
Roman Sinclair <romanio412@gmail.com>
2021-08-09 04:17:42 +0300

    Upgrade to ASP.NET Blazor v5

--- DataAccessLibrary/Interfaces/IEntityRepository.cs
+++ DataAccessLibrary/Interfaces/IEntityRepository.cs
@@ -10,9 +10,11 @@ namespace DataAccessLibrary.Interfaces
         int Delete(Entity entity);
         int Delete(Guid id, string entityName);
 
+        // This GET should be deleted
         IEnumerable<Entity> Get(string entityName, string whereClause = "", int count = 0, params string[] columns);
         IEnumerable<Entity> Get(QueryExpression query);
         
+        // These GetById methods should be deleted
         Entity GetById(string id, string entityName, bool allColumns);
         Entity GetById(EntityReference entityReference, params string[] allColumns);
     }
