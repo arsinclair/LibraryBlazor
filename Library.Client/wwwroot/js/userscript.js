fd61d11ad2381b872ffecdaba8a4ac5eba706ed1
Raman Sinclair <romanio412@gmail.com>
2023-05-16 04:54:16 +0200

    feat: implement auto-population rules for fields using JavaScript

--- Library.Client/wwwroot/js/userscript.js
+++ Library.Client/wwwroot/js/userscript.js
@@ -4,5 +4,15 @@ window.userJsFunctions = {
         if (objDiv != null && objDiv.scrollTop) {
             objDiv.scrollTop = objDiv.scrollHeight;
         }
+    },
+    populateEntityAttributes: function (mappings) {
+        if (mappings && Object.keys(mappings).length > 0) {
+            for (const fieldName of Object.keys(mappings)) {
+                const field = document.querySelector(`input[id="${fieldName}_control" i]`);
+                if (field) {
+                    field.value = mappings[fieldName];
+                }
+            }
+        }
     }
 };
